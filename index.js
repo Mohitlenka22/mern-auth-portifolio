d9dcb55ccc95eada0ab42c526f9f7013d84a9975
mohitlenka <mohitlenka865@gmail.com>
2023-04-06 17:12:02 +0530

    v14

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cookieParser());
 app.use(cors({
     // https://mohitlenka.netlify.app
-    origin: 'https://mohitlenka.netlify.app/',
+    origin: 'https://mohitlenka.netlify.app',
     credentials: true
 }));
 app.use(express.json());
